35039ac94e9652d4d9cfa710654b80d29970bd0d
James Moon <mooniker@gmail.com>
2017-09-02 15:39:07 -0400

    Comment/clean up main functions, revise notice

--- mimic-dict.js
+++ mimic-dict.js
@@ -1,41 +1,40 @@
-// `mimic-dict` is a port/derivative of a Python learning exercise;
-// the original source (as written in Python) is attributed as follows:
-
-// Copyright 2010 Google Inc.
-// Licensed under the Apache License, Version 2.0
-// http://www.apache.org/licenses/LICENSE-2.0
-
-// Google's Python Class
-// http://code.google.com/edu/languages/google-python-class/
+// `mimic-dict.js` is a port/derivative of a Google Python learning exercise.
+// See the included NOTICE text file for details and attribution.
 
 const fs = require('fs')
 const path = require('path')
 
+// Returns mimicDict mapping each word to list of words that follow it
 function mimicDict (filename) {
   let mimicDict = {}
   let text = fs.readFileSync(path.join(__dirname, filename)).toString()
   let words = text.trim().split(/\s+/)
   let prev = ''
   words.forEach(word => {
-    if (!mimicDict[prev]) {
-      mimicDict[prev] = [word]
-    } else {
+    if (mimicDict[prev]) {
       mimicDict[prev].push(word)
+    } else {
+      mimicDict[prev] = [word]
     }
     prev = word
   })
   return mimicDict
 }
 
+// Helper to generate random integer
 function getRandomInt (min, max) {
   min = Math.ceil(min)
   max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min)) + min // The maximum is exclusive and the minimum is inclusive
+  // The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min)) + min
 }
 
+// Given a mimicDict word mapping and start word, return a given
+// number of (or 200) words that randomly follow the mapping
 function printMimic (mimicDict, word, numWords) {
+  numWords = numWords || 200
   let output = word || ''
-  for (var i = 0; i < (numWords || 200); i++) {
+  for (let i = 0; i < numWords; i++) {
     let nexts = mimicDict[word] || mimicDict['']
     word = nexts[getRandomInt(0, nexts.length)]
     output += ' ' + word
